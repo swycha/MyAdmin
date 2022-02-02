98df5707179befd24a726476f9107d95dbc715e3
YoungCha329 <chyoungjj329@gmail.com>
2022-02-02 12:12:10 +0900

    chapter 3

---
+++ src/main/java/com/youngjo/admin/springboot/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.youngjo.admin.springboot.web;
+
+import com.youngjo.admin.springboot.service.posts.PostsService;
+import com.youngjo.admin.springboot.web.dto.PostsResponseDto;
+import com.youngjo.admin.springboot.web.dto.PostsSaveRequestDto;
+import com.youngjo.admin.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
---
+++ src/main/java/com/youngjo/admin/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.youngjo.admin.springboot.web.dto;
+
+import com.youngjo.admin.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
